685b5e4907c6731c371035d2773fdb6f713bf7d8
saraperezal <saraperezaleman1989@gmail.com>
2022-05-17 16:04:09 +0200

    Migrate activity_home to fragment_home

---
+++ app/src/main/java/es/travelWorld/travelling/HomeFragment.java
@@ -0,0 +1,21 @@
+package es.travelworld.travelling;
+
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class HomeFragment extends Fragment {
+
+    public HomeFragment() {
+        super(R.layout.fragment_home);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
